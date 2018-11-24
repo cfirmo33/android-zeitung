af67c40ad5bbc4c886b00d1f631c0935e9d8ab03
Segun Famisa <segunfamisa@gmail.com>
2018-11-25 00:47:02 +0100

    Add main fragment to contain other fragments

---
+++ app/src/main/java/com/segunfamisa/zeitung/MainFragment.kt
@@ -0,0 +1,15 @@
+package com.segunfamisa.zeitung
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+        return view
+    }
+}
