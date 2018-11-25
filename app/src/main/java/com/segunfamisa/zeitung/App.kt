50bb897dfbca982afd2caca81e405f7a1a28a373
Segun Famisa <segunfamisa@gmail.com>
2018-11-25 13:50:18 +0100

    Build app component in application class

--- app/src/main/java/com/segunfamisa/zeitung/App.kt
+++ app/src/main/java/com/segunfamisa/zeitung/App.kt
@@ -1,5 +1,18 @@
 package com.segunfamisa.zeitung
 
 import android.app.Application
+import com.segunfamisa.zeitung.di.AppComponent
+import com.segunfamisa.zeitung.di.DaggerAppComponent
 
-class App : Application()
+class App : Application() {
+
+    lateinit var appComponent: AppComponent
+
+    override fun onCreate() {
+        super.onCreate()
+
+        appComponent = DaggerAppComponent.builder()
+            .withApp(app = this)
+            .build()
+    }
+}
