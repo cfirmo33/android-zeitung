99d82dd96a64a13f597006fa7bb003e73a4d7dbd
Segun Famisa <segunfamisa@gmail.com>
2018-11-25 14:52:56 +0100

    Bind coroutines dispatcher provider interface to the default provider

--- app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
+++ app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
@@ -4,13 +4,24 @@ import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
 import com.segunfamisa.zeitung.data.di.DataModule
+import com.segunfamisa.zeitung.util.CoroutineDispatcherProvider
+import com.segunfamisa.zeitung.util.DefaultCoroutineDispatcherProvider
+import dagger.Binds
 import dagger.Module
 import dagger.Provides
 
 @Module(includes = [DataModule::class])
-class AppModule {
+abstract class AppModule {
 
-    @Provides
-    fun provideSharedPreferences(application: Application): SharedPreferences =
-        application.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+    @Binds
+    abstract fun bindCoroutinesDispatcher(dispatcherProvider: DefaultCoroutineDispatcherProvider): CoroutineDispatcherProvider
+
+    @Module
+    companion object {
+
+        @Provides
+        @JvmStatic
+        fun provideSharedPreferences(application: Application): SharedPreferences =
+            application.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
+    }
 }
