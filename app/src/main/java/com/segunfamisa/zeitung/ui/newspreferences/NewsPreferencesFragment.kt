fd7d1fe4a27b4ce5f6e2b8502be263a3b80bf045
Segun Famisa <segunfamisa@gmail.com>
2018-11-25 13:51:15 +0100

    Rename fragment

--- app/src/main/java/com/segunfamisa/zeitung/ui/newspreferences/NewsPreferencesFragment.kt
+++ app/src/main/java/com/segunfamisa/zeitung/ui/newspreferences/NewsPreferencesFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.segunfamisa.zeitung.R
 
-class NewsPreferences : Fragment() {
+class NewsPreferencesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
