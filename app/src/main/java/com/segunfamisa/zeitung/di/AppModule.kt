65f2968f744d9d25892a85f221a8dd90141652d1
Segun Famisa <segunfamisa@gmail.com>
2018-11-25 02:43:05 +0100

    Add SharedPreferences to dagger graph

--- app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
+++ app/src/main/java/com/segunfamisa/zeitung/di/AppModule.kt
@@ -1,9 +1,16 @@
 package com.segunfamisa.zeitung.di
 
+import android.app.Application
+import android.content.Context
+import android.content.SharedPreferences
 import com.segunfamisa.zeitung.data.di.DataModule
 import dagger.Module
+import dagger.Provides
 
 @Module(includes = [DataModule::class])
 class AppModule {
 
+    @Provides
+    fun provideSharedPreferences(application: Application): SharedPreferences =
+        application.getSharedPreferences("user_prefs", Context.MODE_PRIVATE)
 }
